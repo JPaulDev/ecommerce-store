2df21b58573040a04f0adec2c63b4ec9e94bbbce
JonPaulCode <jwatson88@live.co.uk>
2022-08-27 14:16:21 +0100

    Add tests for header links

---
+++ src/components/Header/Links/__tests__/Links.test.jsx
@@ -0,0 +1,28 @@
+import renderer from 'react-test-renderer';
+import Links from '../index';
+
+afterAll(() => {
+  window.resizeTo(1024, 768); // Jest default window size
+});
+
+describe('Links component at a screen width of 575px and above', () => {
+  beforeAll(() => {
+    window.resizeTo(575, 768);
+  });
+
+  it('shows links text and icons', () => {
+    const tree = renderer.create(<Links />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Links component at a screen width of 574px and below', () => {
+  beforeAll(() => {
+    window.resizeTo(574, 768);
+  });
+
+  it('hides links text leaving only icons visible', () => {
+    const tree = renderer.create(<Links />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
