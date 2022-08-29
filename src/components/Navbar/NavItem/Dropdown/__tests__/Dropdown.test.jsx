5ca35c95cd8ccf3bbec06f292fd63f3497984e4f
JonPaulCode <jwatson88@live.co.uk>
2022-08-29 15:09:21 +0100

    Add dropdown menu tests

---
+++ src/components/Navbar/NavItem/Dropdown/__tests__/Dropdown.test.jsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Dropdown from '../index';
+
+afterAll(() => {
+  window.resizeTo(1024, 768); // Jest default window size
+});
+
+describe('components dropdown menu', () => {
+  it('It renders correctly and all links/advertisements are present', () => {
+    const tree = renderer.create(<Dropdown menu="Components" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('accessories dropdown menu', () => {
+  it('It renders correctly and all links/advertisements are present', () => {
+    const tree = renderer.create(<Dropdown menu="Accessories" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Dropdown component at a screen width of 925px and above', () => {
+  it('shows advertisements in the dropdown', () => {
+    window.resizeTo(925, 768);
+    render(<Dropdown menu="Components" />);
+
+    expect(screen.getAllByRole('img')).toHaveLength(3);
+  });
+});
+
+describe('Dropdown component at a screen width of 924px and below', () => {
+  it('hides advertisements in the dropdown', () => {
+    window.resizeTo(924, 768);
+    render(<Dropdown menu="Components" />);
+
+    expect(screen.queryByRole('img')).toBeNull();
+  });
+});
