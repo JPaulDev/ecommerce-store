b5e6647cd735ed02599ab82dbb1c92d15a2cb737
JonPaulCode <jwatson88@live.co.uk>
2022-08-31 19:09:58 +0100

    Add titles and aria labels to carousel nav arrows

--- src/pages/Home/Carousel/NavArrows/index.jsx
+++ src/pages/Home/Carousel/NavArrows/index.jsx
@@ -4,10 +4,20 @@ import * as Styled from './styles';
 export default function NavArrows({ onChangeSlide }) {
   return (
     <>
-      <Styled.PrevSlideBtn type="button" onClick={() => onChangeSlide('PREV')}>
+      <Styled.PrevSlideBtn
+        type="button"
+        title="Previous"
+        aria-label="Previous Slide"
+        onClick={() => onChangeSlide('PREV')}
+      >
         <MaterialIcon iconSize="32px">chevron_left</MaterialIcon>
       </Styled.PrevSlideBtn>
-      <Styled.NextSlideBtn type="button" onClick={() => onChangeSlide('NEXT')}>
+      <Styled.NextSlideBtn
+        type="button"
+        title="Next"
+        aria-label="Next Slide"
+        onClick={() => onChangeSlide('NEXT')}
+      >
         <MaterialIcon iconSize="32px">chevron_right</MaterialIcon>
       </Styled.NextSlideBtn>
     </>
