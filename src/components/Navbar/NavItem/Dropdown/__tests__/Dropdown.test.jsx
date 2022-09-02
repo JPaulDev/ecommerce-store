406365be87c3049dfe37bee37fde3d671b302be1
JonPaulCode <jwatson88@live.co.uk>
2022-09-02 13:02:23 +0100

    Refactor navbar tests

--- src/components/Navbar/NavItem/Dropdown/__tests__/Dropdown.test.jsx
+++ src/components/Navbar/NavItem/Dropdown/__tests__/Dropdown.test.jsx
@@ -2,22 +2,20 @@ import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Dropdown from '../index';
 
-describe('components dropdown menu', () => {
-  it('It renders correctly and all links/advertisements are present', () => {
+describe('Dropdown component', () => {
+  it('renders the components menu with all links/advertisements present', () => {
     const tree = renderer.create(<Dropdown menu="Components" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-});
 
-describe('accessories dropdown menu', () => {
-  it('It renders correctly and all links/advertisements are present', () => {
+  it('renders the accessories menu with all links/advertisements present', () => {
     const tree = renderer.create(<Dropdown menu="Accessories" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
 
 describe('Dropdown component at a screen width of 925px and above', () => {
-  it('shows advertisements in the dropdown', () => {
+  it('shows advertisements in the dropdown menu', () => {
     window.resizeTo(925, 768);
     render(<Dropdown menu="Components" />);
 
@@ -26,7 +24,7 @@ describe('Dropdown component at a screen width of 925px and above', () => {
 });
 
 describe('Dropdown component at a screen width of 924px and below', () => {
-  it('hides advertisements in the dropdown', () => {
+  it('hides advertisements in the dropdown menu', () => {
     window.resizeTo(924, 768);
     render(<Dropdown menu="Components" />);
 
--- src/components/Navbar/__tests__/Navbar.test.jsx
+++ src/components/Navbar/__tests__/Navbar.test.jsx
@@ -1,7 +1,9 @@
 import renderer from 'react-test-renderer';
 import Navbar from '../index';
 
-it('Navbar renders correctly and all links are present', () => {
-  const tree = renderer.create(<Navbar />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Navbar component', () => {
+  it('renders all links and buttons', () => {
+    const tree = renderer.create(<Navbar />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
