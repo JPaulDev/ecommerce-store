c00c25908e14b23a7fc3e7ddd124b2ecb2f856d1
JonPaulCode <jwatson88@live.co.uk>
2022-09-04 10:57:18 +0100

    Change border radius on link button

--- src/components/LinkButtonGrey.js
+++ src/components/LinkButtonGrey.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const LinkButtonGrey = styled.a`
   background: linear-gradient(to top, #858484, #a0a0a0);
-  border-radius: 3px;
+  border-radius: 5px;
   color: white;
   display: inline-block;
   font-size: 0.8rem;
