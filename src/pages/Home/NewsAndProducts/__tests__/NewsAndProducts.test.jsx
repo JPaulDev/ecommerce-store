29997c6864e95acd7464706e8d76d3bbdcb2e73c
JonPaulCode <jwatson88@live.co.uk>
2022-09-11 15:27:49 +0100

    Add news and latest products section tests

---
+++ src/pages/Home/NewsAndProducts/__tests__/NewsAndProducts.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import NewsAndProducts from '../index';
+
+describe('NewsAndProducts component', () => {
+  it('renders a news section with 3 articles and latest products links', () => {
+    const tree = renderer.create(<NewsAndProducts />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('NewsAndProducts component at different screen widths', () => {
+  it('renders the latest products section 1150px and above', () => {
+    window.resizeTo(1050, 768);
+    render(<NewsAndProducts />);
+    const heading = screen.getByRole('heading', {
+      name: /hot & new products/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('does not render the latest products section 1149px and below', () => {
+    window.resizeTo(1049, 768);
+    render(<NewsAndProducts />);
+    const heading = screen.queryByRole('heading', {
+      name: /hot & new products/i,
+    });
+
+    expect(heading).toBeNull();
+  });
+});
