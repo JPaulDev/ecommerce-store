4996e0bfe73337b4a8b6e4162a48dc1c2671fd76
JonPaulCode <jwatson88@live.co.uk>
2022-10-11 10:54:27 +0100

    Add title to 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,22 @@
+import Head from 'next/head';
 import Image from 'next/future/image';
-import image from '../../public/images/404/404-error.webp';
+import image404 from '../../public/images/404/404-error.webp';
 import * as Styled from '../styles/404';
 
 export default function NotFound() {
   return (
-    <Styled.Container>
-      <Image src={image} alt="" quality={85} priority="true" />
-      <h1>Not Found</h1>
-      <p>
-        The requested page doesn&lsquo;t exist or you don&lsquo;t have access to
-        it.
-      </p>
-    </Styled.Container>
+    <>
+      <Head>
+        <title>404: Page Not Found</title>
+      </Head>
+      <Styled.Container>
+        <Image src={image404} alt="" quality={85} priority="true" />
+        <h1>Not Found</h1>
+        <p>
+          The requested page doesn&lsquo;t exist or you don&lsquo;t have access
+          to it.
+        </p>
+      </Styled.Container>
+    </>
   );
 }
