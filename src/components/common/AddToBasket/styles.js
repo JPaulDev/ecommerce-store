d7ffc85df77f371ba2756ca886425b73ec061477
JonPaulCode <jwatson88@live.co.uk>
2022-10-20 18:44:59 +0100

    Adjust add to basket button styles

--- src/components/common/AddToBasket/styles.js
+++ src/components/common/AddToBasket/styles.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   background: linear-gradient(to top, #3b7739, #55a952);
-  border-radius: 5px;
-  box-shadow: 0 3px 8px 0 rgb(213 217 217 / 70%);
+  border-radius: 6px;
+  box-shadow: var(--shadow-button);
   color: white;
   cursor: pointer;
   font-size: ${({ $fontSize }) => $fontSize};
   font-weight: 700;
   margin-top: 8px;
-  min-height: 40px;
+  padding: 12px 0;
   text-transform: uppercase;
   width: ${({ $width }) => $width};
 
@@ -17,10 +17,13 @@ export const Button = styled.button`
     background: var(--color-grey-12);
     color: rgb(255 255 255 / 90%);
     cursor: initial;
+    box-shadow: none;
+    pointer-events: none;
   }
 
   &:not(:disabled):hover {
     background: linear-gradient(to top, #326830, #4a9b47);
+    box-shadow: var(--shadow-button-hover);
   }
 
   &:not(:disabled):active {
