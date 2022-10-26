b37cd0e5c71f8086843681aecfabd74616af6716
JonPaulCode <jwatson88@live.co.uk>
2022-10-26 18:46:39 +0100

    Fix fonts being improperly cached

--- next.config.js
+++ next.config.js
@@ -18,6 +18,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(ttf|otf|woff|woff2)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
