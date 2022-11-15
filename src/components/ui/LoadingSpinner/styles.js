5a5e86ba457e2f0f7a2cbd9ad8685474cf788520
JonPaulCode <jwatson88@live.co.uk>
2022-11-15 14:18:44 +0000

    Remove duplicate style from loading spinner

--- src/components/ui/LoadingSpinner/styles.js
+++ src/components/ui/LoadingSpinner/styles.js
@@ -20,7 +20,6 @@ export const Spinner = styled.div`
   animation: 0.75s linear infinite ${rotate};
   border: 0.2em solid currentColor;
   border-radius: 50%;
-  border-radius: 50%;
   border-right-color: transparent;
   height: 1.125em;
   margin-right: 5px;
